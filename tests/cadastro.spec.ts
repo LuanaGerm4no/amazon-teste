7f4d7dd4247b477d4d95de7d966628fb0dbc093a
LuanaGerm4no <lgbc@discente.ifpe.edu.br>
2024-09-09 23:13:28 -0300

    adicionando testes de cadastro

--- tests/cadastro.spec.ts
+++ tests/cadastro.spec.ts
@@ -11,14 +11,18 @@ test('verificacao da etapa de registro com credenciais validas', async ({ page }
 
 });
 
-test('verificacao da etapa de  proteção de conta', async ({ page }) => {
+test('Visualizar Condicoes de uso', async ({ page }) => {
   await page.goto('https://www.amazon.com.br/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.br%2F%3F%26tag%3Dhydrbrabk-20%26ref%3Dnav_signin%26adgrpid%3D155790195778%26hvpone%3D%26hvptwo%3D%26hvadid%3D677606588104%26hvpos%3D%26hvnetw%3Dg%26hvrand%3D16410208552389436964%26hvqmt%3De%26hvdev%3Dc%26hvdvcmdl%3D%26hvlocint%3D%26hvlocphy%3D9101384%26hvtargid%3Dkwd-10573980%26hydadcr%3D26346_11691057%26gad_source%3D1&prevRID=VA4J3QW3H7YXEC1NVHCQ&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=brflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=brflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
 
+  await page.locator('#legalTextRow').getByRole('link', { name: 'Condições de Uso' }).click();
+  await expect(page).toHaveTitle(/Condições de Uso - Serviço de atendimento ao cliente da Amazon/);
 });
 
-test('Verificação da etapa de confirmação de email', async ({ page }) => {
+test('Fazer login da tela de registro', async ({ page }) => {
   await page.goto('https://www.amazon.com.br/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.br%2F%3F%26tag%3Dhydrbrabk-20%26ref%3Dnav_signin%26adgrpid%3D155790195778%26hvpone%3D%26hvptwo%3D%26hvadid%3D677606588104%26hvpos%3D%26hvnetw%3Dg%26hvrand%3D16410208552389436964%26hvqmt%3De%26hvdev%3Dc%26hvdvcmdl%3D%26hvlocint%3D%26hvlocphy%3D9101384%26hvtargid%3Dkwd-10573980%26hydadcr%3D26346_11691057%26gad_source%3D1&prevRID=VA4J3QW3H7YXEC1NVHCQ&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=brflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=brflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
 
+  await page.getByRole('link', { name: 'Fazer login' }).click();
+  await expect(page).toHaveTitle(/Acessar Amazon/);
 });
 
 test('Registro com senha fraca', async ({ page }) => {
